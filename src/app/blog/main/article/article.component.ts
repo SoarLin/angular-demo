7548688b211ec8ef66627f326669b1c6b1f25a99
Soar Lin <Soar.Lin@ruckuswireless.com>
2020-08-07 15:02:18 +0800

    Add article title edit function

--- src/app/blog/main/article/article.component.ts
+++ src/app/blog/main/article/article.component.ts
@@ -8,9 +8,32 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ArticleComponent implements OnInit {
   @Input() post: any;
   @Output() openLink = new EventEmitter<any>();
+  @Output() updateTitle = new EventEmitter<any>();
+
+  isEdit = false;
+  editTitle: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  editArticleTitle(): void {
+    this.isEdit = true;
+    this.editTitle = this.post.title;
+  }
+
+  exitEditTitle(): void {
+    this.isEdit = false;
+  }
+
+  saveTitle(): void {
+    this.updateTitle.emit({
+      id: this.post.id,
+      title: this.editTitle
+    });
+    this.exitEditTitle();
   }
 
   clickMore(link: string): void {
